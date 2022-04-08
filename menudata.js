28fb2377234a9cff771cf25dba249a7a625749ab
rem1776 <rem1776@users.noreply.github.com>
2022-04-08 20:00:34 +0000

    deploy: 3d7bbea4d600206f49f006d265b27157cb9a72a3

--- menudata.js
+++ menudata.js
@@ -22,7 +22,7 @@ for the JavaScript code in this file
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
-{text:"Related Pages",url:"pages.html"},
+{text:"Namelist Configuration",url:"pages.html"},
 {text:"Couplers and Modules",url:"modules.html"},
 {text:"Interfaces and Derived Types",url:"annotated.html",children:[
 {text:"By Module",url:"annotated.html"},
